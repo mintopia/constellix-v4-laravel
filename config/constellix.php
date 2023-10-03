de804929a780dd2400619568f636e4c1e41b8fb8
Jessica Smith <jessica.smith@digicert.com>
2023-10-03 10:57:45 +0100

    Update to fix various issues found during integration testing

--- config/constellix.php
+++ config/constellix.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'endpoint' => env('CONSTELLIX_API_ENDPOINT', 'https://api.dns.constellix.com/v4'),
-    'secret' => env('CONSTELLIX_API_SECRET'),
+    'secret_key' => env('CONSTELLIX_API_SECRET'),
     'api_key' => env('CONSTELLIX_API_KEY'),
 ];
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,13 +5,12 @@ namespace Tiggee\ConstellixApiLaravel;
 use Constellix\Client\Client;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
-use Psr\Http\Client\ClientInterface;
 use Psr\Log\LoggerInterface;
 use Tiggee\ConstellixApiLaravel\Contracts\ConstellixApi;
 
 class ServiceProvider extends LaravelServiceProvider
 {
-    protected function boot(): void
+    public function boot(): void
     {
         $this->publishes([
             __DIR__ . '/../config/constellix.php' => config_path('constellix.php'),
@@ -22,7 +21,7 @@ class ServiceProvider extends LaravelServiceProvider
     {
         $this->app->bind(abstract: ConstellixApi::class, concrete: function (Application $app) {
             $client = new Client(
-                client: $app->make(ClientInterface::class),
+                client: new \GuzzleHttp\Client(),
                 paginatorFactory: new IlluminatePaginatorFactory(),
                 logger: $app->make(LoggerInterface::class)
             );
